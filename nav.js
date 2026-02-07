31040a6474e38d94034a0c9b7c68ef042eea70cd
gk-lnu <gustav.1.koser@gmail.com>
2026-02-07 21:09:24 +0100

    lägg till uppgifter i nav

--- nav.js
+++ nav.js
@@ -4,12 +4,21 @@ export function createNav() {
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
   
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  a.href = 'index.html';
-  a.textContent = 'Hem';
-  li.append(a);
-  ul.append(li);
+  const homeLi = document.createElement('li');
+  const homeA = document.createElement('a');
+  homeA.href = 'index.html';
+  homeA.textContent = 'Hem';
+  homeLi.append(homeA);
+  ul.append(homeLi);
+  
+  for (const assignment of assignments) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = assignment.link;
+    a.textContent = assignment.title;
+    li.append(a);
+    ul.append(li);
+  }
   
   nav.append(ul);
   return nav;
