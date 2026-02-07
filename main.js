5f0ba1c1f7c17ae9473807abd1c73d3a6cc58207
gk-lnu <gustav.1.koser@gmail.com>
2026-02-07 21:09:24 +0100

    funktion för kort

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import { assignments } from './assignments.js';
 
+function createCard(assignment) {
+  const div = document.createElement('div');
+  div.classList.add('card');
+  
+  const h3 = document.createElement('h3');
+  h3.textContent = assignment.title;
+  
+  div.append(h3);
+  return div;
+}
+
 const container = document.getElementById('cards');
 
 for (const a of assignments) {
-  const div = document.createElement('div');
-  div.textContent = a.title;
-  container.append(div);
+  const card = createCard(a);
+  container.append(card);
 }
