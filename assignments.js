5f03efe911db783fdf77db3d29384b7e19c6ea0a
gk-lnu <gustav.1.koser@gmail.com>
2026-02-07 21:09:25 +0100

    lägg till uppgift 2

--- assignments.js
+++ assignments.js
@@ -2,7 +2,13 @@ export const assignments = [
   {
     id: "assignment1",
     title: "Samlingssida för inlämningsuppgifter",
-    description: "Skapa repository med landningssida",
+    description: "Skapa ett repository med en landningssida som innehåller dynamisk navigation och kort för alla uppgifter.",
     link: "assignment1/index.html"
+  },
+  {
+    id: "assignment2",
+    title: "Produktsida med kundvagn",
+    description: "Bygg en produktsida med möjlighet att lägga till varor i en kundvagn som sparas i localStorage.",
+    link: "assignment2/index.html"
   }
 ];
