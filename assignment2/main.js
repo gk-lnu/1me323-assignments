2b53ec41dd2026efdfdc7f3a8d7a974d748d648f
gk-lnu <gustav.1.koser@gmail.com>
2026-02-13 10:50:00 +0100

    template literals

--- assignment2/main.js
+++ assignment2/main.js
@@ -5,10 +5,10 @@ function renderProducts() {
   let html = '';
   
   for (const product of products) {
-    html += '<div class="product-card">';
-    html += '<h3>' + product.name + '</h3>';
-    html += '<p>' + product.price + ' kr</p>';
-    html += '</div>';
+    html += `<div class="product-card">
+      <h3>${product.name}</h3>
+      <p>${product.price} kr</p>
+    </div>`;
   }
   
   grid.innerHTML = html;
