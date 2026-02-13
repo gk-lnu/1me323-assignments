1547f5f947d4dac4fd0a2576a6241858ac8d756a
gk-lnu <gustav.1.koser@gmail.com>
2026-02-13 08:50:00 +0100

    lägg till category

--- assignment2/products.js
+++ assignment2/products.js
@@ -3,12 +3,14 @@ export const products = [
     id: 1,
     name: "Smartphone S1",
     description: "Kraftfull smartphone",
-    price: 12500
+    price: 12500,
+    category: "Mobil"
   },
   {
     id: 2,
     name: "Laptop Pro 15",
     description: "Tunn laptop",
-    price: 17299
+    price: 17299,
+    category: "Datorer"
   }
 ];
