dacf758403fd93004b9582fde75c46adc8633b7f
gk-lnu <gustav.1.koser@gmail.com>
2026-02-13 08:42:00 +0100

    lägg till description

--- assignment2/products.js
+++ assignment2/products.js
@@ -2,11 +2,13 @@ export const products = [
   {
     id: 1,
     name: "Smartphone S1",
+    description: "Kraftfull smartphone",
     price: 12500
   },
   {
     id: 2,
     name: "Laptop Pro 15",
+    description: "Tunn laptop",
     price: 17299
   }
 ];
