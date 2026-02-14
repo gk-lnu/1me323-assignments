e8b935d082312b754f6147a2667475b3f347e49e
gk-lnu <gustav.1.koser@gmail.com>
2026-02-14 10:32:00 +0100

    uppdatera assignments.js

--- assignments.js
+++ assignments.js
@@ -1,14 +1,12 @@
 export const assignments = [
   {
-    id: "assignment1",
-    title: "Samlingssida för inlämningsuppgifter",
-    description: "Skapa ett repository med en landningssida som innehåller dynamisk navigation och kort för alla uppgifter.",
-    link: "assignment1/index.html"
+    title: "Assignment 1",
+    description: "Personlig presentation med responsiv design och dynamisk navigation.",
+    link: "assignment1/"
   },
   {
-    id: "assignment2",
-    title: "Produktsida med kundvagn",
-    description: "Bygg en produktsida med möjlighet att lägga till varor i en kundvagn som sparas i localStorage.",
-    link: "assignment2/index.html"
+    title: "Assignment 2",
+    description: "Produktsida med kundvagn och localStorage.",
+    link: "assignment2/"
   }
 ];
