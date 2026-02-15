17ec137b86fb5afa5b98eda05cd95972fabbdbe5
gk-lnu <gustav.1.koser@gmail.com>
2026-02-15 05:35:03 +0100

    ändra till svenska

--- assignments.js
+++ assignments.js
@@ -1,11 +1,11 @@
 export const assignments = [
   {
-    title: "Assignment 1",
+    title: "Uppgift 1",
     description: "Personlig presentation med responsiv design och dynamisk navigation.",
     link: "assignment1/"
   },
   {
-    title: "Assignment 2",
+    title: "Uppgift 2",
     description: "Produktsida med kundvagn och localStorage.",
     link: "assignment2/"
   }
