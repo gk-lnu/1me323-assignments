5178c027477ee85dfc8da9ab168f3531e971b6dc
gk-lnu <gustav.1.koser@gmail.com>
2026-03-01 21:48:00 +0100

    lägg till privata fält och getters

--- assignment3/Match.js
+++ assignment3/Match.js
@@ -1,7 +1,24 @@
 // klass för en match
 export class Match {
+  #deltagare1;
+  #deltagare2;
+  #vinnare;
+
   constructor(deltagare1, deltagare2) {
-    this.deltagare1 = deltagare1;
-    this.deltagare2 = deltagare2;
+    this.#deltagare1 = deltagare1;
+    this.#deltagare2 = deltagare2;
+    this.#vinnare = null;
+  }
+
+  get deltagare1() {
+    return this.#deltagare1;
+  }
+
+  get deltagare2() {
+    return this.#deltagare2;
+  }
+
+  get vinnare() {
+    return this.#vinnare;
   }
 }
