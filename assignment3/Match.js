13f3cb864dca182fc76e3682b3b3cba2b48853ce
gk-lnu <gustav.1.koser@gmail.com>
2026-03-02 13:15:00 +0100

    lägg till spela metod med slump

--- assignment3/Match.js
+++ assignment3/Match.js
@@ -4,12 +4,16 @@ export class Match {
   #deltagare2;
   #vinnare;
   #element;
+  #d1Element;
+  #d2Element;
 
   constructor(deltagare1, deltagare2) {
     this.#deltagare1 = deltagare1;
     this.#deltagare2 = deltagare2;
     this.#vinnare = null;
     this.#element = null;
+    this.#d1Element = null;
+    this.#d2Element = null;
   }
 
   get deltagare1() {
@@ -24,6 +28,34 @@ export class Match {
     return this.#vinnare;
   }
 
+  spela() {
+    if (this.#vinnare !== null) return;
+    
+    const skill1 = this.#deltagare1.skillLevel ?? 5;
+    const skill2 = this.#deltagare2.skillLevel ?? 5;
+    const chans1 = skill1 / (skill1 + skill2);
+    
+    if (Math.random() < chans1) {
+      this.#vinnare = this.#deltagare1;
+    } else {
+      this.#vinnare = this.#deltagare2;
+    }
+    
+    this.#uppdateraElement();
+  }
+
+  #uppdateraElement() {
+    if (!this.#d1Element || !this.#d2Element) return;
+    
+    if (this.#vinnare === this.#deltagare1) {
+      this.#d1Element.classList.add("vinnare");
+      this.#d2Element.classList.add("förlorare");
+    } else {
+      this.#d1Element.classList.add("förlorare");
+      this.#d2Element.classList.add("vinnare");
+    }
+  }
+
   skapaElement() {
     const div = document.createElement("div");
     div.classList.add("match");
@@ -31,10 +63,12 @@ export class Match {
     const d1 = document.createElement("div");
     d1.classList.add("deltagare");
     d1.textContent = this.#deltagare1.name;
+    this.#d1Element = d1;
     
     const d2 = document.createElement("div");
     d2.classList.add("deltagare");
     d2.textContent = this.#deltagare2.name;
+    this.#d2Element = d2;
     
     div.append(d1, d2);
     this.#element = div;
