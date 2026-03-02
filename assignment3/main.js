ee02b0e91c32847c4fa6f0ac49fd7de7b5e1b027
gk-lnu <gustav.1.koser@gmail.com>
2026-03-02 10:15:00 +0100

    hämta deltagare från json

--- assignment3/main.js
+++ assignment3/main.js
@@ -1,2 +1,15 @@
 // huvudscript
-console.log("main.js laddat");
+import { Match } from "./Match.js";
+
+async function hämtaDeltagare() {
+  const response = await fetch("contestants.json");
+  const data = await response.json();
+  return data;
+}
+
+async function init() {
+  const deltagare = await hämtaDeltagare();
+  console.log(deltagare);
+}
+
+init();
