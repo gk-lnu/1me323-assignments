ed98743653769a798f98c80e161b03a2f2d78f42
gk-lnu <gustav.1.koser@gmail.com>
2026-03-02 18:35:00 +0100

    lägg till assignment3 i listan

--- assignments.js
+++ assignments.js
@@ -1,12 +1,21 @@
+// datastruktur för alla uppgifter
 export const assignments = [
   {
+    id: "assignment1",
     title: "Uppgift 1",
-    description: "Personlig presentation med responsiv design och dynamisk navigation.",
-    link: "assignment1/"
+    description: "Samlingssida för inlämningsuppgifter.",
+    link: "assignment1/index.html"
   },
   {
+    id: "assignment2",
     title: "Uppgift 2",
     description: "Produktsida med kundvagn och localStorage.",
-    link: "assignment2/"
+    link: "assignment2/index.html"
+  },
+  {
+    id: "assignment3",
+    title: "Uppgift 3",
+    description: "Turneringsgenerator med bracket-system.",
+    link: "assignment3/index.html"
   }
 ];
