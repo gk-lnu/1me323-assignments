c4eaa46ca426a68423d7a30d7f30dc4b36bab9b3
gk-lnu <gustav.1.koser@gmail.com>
2026-03-02 11:35:00 +0100

    lägg till skapaElement metod

--- assignment3/Match.js
+++ assignment3/Match.js
@@ -3,11 +3,13 @@ export class Match {
   #deltagare1;
   #deltagare2;
   #vinnare;
+  #element;
 
   constructor(deltagare1, deltagare2) {
     this.#deltagare1 = deltagare1;
     this.#deltagare2 = deltagare2;
     this.#vinnare = null;
+    this.#element = null;
   }
 
   get deltagare1() {
@@ -20,5 +22,22 @@ export class Match {
 
   get vinnare() {
     return this.#vinnare;
+  }
+
+  skapaElement() {
+    const div = document.createElement("div");
+    div.classList.add("match");
+    
+    const d1 = document.createElement("div");
+    d1.classList.add("deltagare");
+    d1.textContent = this.#deltagare1.name;
+    
+    const d2 = document.createElement("div");
+    d2.classList.add("deltagare");
+    d2.textContent = this.#deltagare2.name;
+    
+    div.append(d1, d2);
+    this.#element = div;
+    return div;
   }
 }
