c2bd637a2275cc905ef584a37cbf6b351d961e57
gk-lnu <gustav.1.koser@gmail.com>
2026-03-19 19:31:14 +0100

    fetchHouses i utils.js

--- assignment4/js/utils.js
+++ assignment4/js/utils.js
@@ -1,1 +1,8 @@
-// Hjälpfunktioner
+export async function fetchHouses() {
+  const response = await fetch("data/houses.json");
+  if (!response.ok) {
+    throw new Error("Kunde inte hämta husdata");
+  }
+  const houses = await response.json();
+  return houses;
+}
