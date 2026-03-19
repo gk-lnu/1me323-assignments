9b114a8a1f4e44c28b7346ee2cde08d05a5b102e
gk-lnu <gustav.1.koser@gmail.com>
2026-03-19 21:35:05 +0100

    starta index.js med init

--- assignment4/js/index.js
+++ assignment4/js/index.js
@@ -1,1 +1,16 @@
-// Översiktssidan
+import { fetchHouses, getScareText, showError } from "./utils.js";
+
+const houseContainer = document.getElementById("house-list");
+
+let allHouses = [];
+
+async function init() {
+  try {
+    allHouses = await fetchHouses();
+    renderHouses(allHouses);
+  } catch (error) {
+    showError(houseContainer, "Kunde inte ladda husen. Försök igen senare.");
+  }
+}
+
+init();
