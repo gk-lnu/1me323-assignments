9e85b2604cd1628bfe2a1db61788b6635ab53074
gk-lnu <gustav.1.koser@gmail.com>
2026-03-19 20:08:47 +0100

    getScareText funktion

--- assignment4/js/utils.js
+++ assignment4/js/utils.js
@@ -6,3 +6,12 @@ export async function fetchHouses() {
   const houses = await response.json();
   return houses;
 }
+
+export function getScareText(level) {
+  if (level === 1) return "Mysigt";
+  if (level === 2) return "Lite läskigt";
+  if (level === 3) return "Obehagligt";
+  if (level === 4) return "Skräckinjagande";
+  if (level === 5) return "Ren terror";
+  return "Okänd";
+}
