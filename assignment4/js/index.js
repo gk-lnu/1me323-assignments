b31347d22e0b750f13de0bac14bc116423b0f0d5
gk-lnu <gustav.1.koser@gmail.com>
2026-03-19 22:39:18 +0100

    renderHouses med innerHTML

--- assignment4/js/index.js
+++ assignment4/js/index.js
@@ -13,4 +13,25 @@ async function init() {
   }
 }
 
+function renderHouses(houses) {
+  houseContainer.innerHTML = "";
+
+  for (const house of houses) {
+    const card = document.createElement("article");
+    card.className = "house-card";
+
+    card.innerHTML =
+      '<img src="images/' + house.image + '" alt="' + house.name + '">' +
+      '<div class="card-content">' +
+        '<h2>' + house.name + '</h2>' +
+        '<p class="location">' + house.location + '</p>' +
+        '<p class="price">' + house.pricePerNight + ' kr/natt</p>' +
+        '<p class="scare">Skräcknivå: ' + getScareText(house.scareLevel) + '</p>' +
+        '<a href="house.html?id=' + house.id + '" class="btn">Läs mer och boka</a>' +
+      '</div>';
+
+    houseContainer.appendChild(card);
+  }
+}
+
 init();
