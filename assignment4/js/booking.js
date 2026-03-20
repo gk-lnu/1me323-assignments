c47d2049de9097ad9d899d740d8a767b0de2c298
gk-lnu <gustav.1.koser@gmail.com>
2026-03-20 18:12:33 +0100

    calculateTotal och hasDiscount

--- assignment4/js/booking.js
+++ assignment4/js/booking.js
@@ -32,4 +32,30 @@ export default class Booking {
   setExtra(name, checked) {
     this.#extras[name] = checked;
   }
+
+  get hasDiscount() {
+    return this.#promoCode.toUpperCase() === "GHOST20";
+  }
+
+  calculateTotal() {
+    let total = this.#house.pricePerNight * this.#days;
+
+    if (this.#extras.breakfast) {
+      total += 100 * this.#days;
+    }
+
+    if (this.#extras.ghostTour) {
+      total += 300;
+    }
+
+    if (this.#extras.seance) {
+      total += 500;
+    }
+
+    if (this.hasDiscount) {
+      total = total * 0.8;
+    }
+
+    return total;
+  }
 }
