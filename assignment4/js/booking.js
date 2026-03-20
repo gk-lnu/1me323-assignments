713ac807818f17d6a106641943faf2a48232bbd8
gk-lnu <gustav.1.koser@gmail.com>
2026-03-20 17:55:48 +0100

    Booking klass med constructor och setters

--- assignment4/js/booking.js
+++ assignment4/js/booking.js
@@ -1,1 +1,35 @@
-// Bokningsklass
+export default class Booking {
+  #house;
+  #checkIn;
+  #days;
+  #extras;
+  #promoCode;
+
+  constructor(house) {
+    this.#house = house;
+    this.#checkIn = "";
+    this.#days = 1;
+    this.#extras = {
+      breakfast: false,
+      ghostTour: false,
+      seance: false
+    };
+    this.#promoCode = "";
+  }
+
+  set checkIn(value) {
+    this.#checkIn = value;
+  }
+
+  set days(value) {
+    this.#days = Number(value);
+  }
+
+  set promoCode(value) {
+    this.#promoCode = value.trim();
+  }
+
+  setExtra(name, checked) {
+    this.#extras[name] = checked;
+  }
+}
