0a14680724a65fbcec3699a5445712ac29becfac
gk-lnu <gustav.1.koser@gmail.com>
2026-03-20 17:10:33 +0100

    starta house.js med URLSearchParams

--- assignment4/js/house.js
+++ assignment4/js/house.js
@@ -1,1 +1,10 @@
-// Detaljsidan
+import { fetchHouses, getScareText, showError } from "./utils.js";
+
+const houseDetail = document.getElementById("house-detail");
+
+async function init() {
+  const params = new URLSearchParams(window.location.search);
+  const id = Number(params.get("id"));
+}
+
+init();
